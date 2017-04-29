fab3bf7a72af42699a6f634d1776c73892c24495
Dennis Hamester <dennis.hamester@startmail.com>
2017-04-29 14:33:25 +0200

    instance: Use VkInstanceCreateInfoWrapper in create

--- src/core/instance.rs
+++ src/core/instance.rs
@@ -26,83 +26,7 @@ use vk_sys;
 pub struct Instance(Arc<InstanceHandle>);
 
 impl Instance {
-    pub fn create(mut create_info: core::InstanceCreateInfo, allocator: Option<Box<core::Allocator>>) -> Result<Instance> {
-        let mut application_name_cstr = None;
-        let mut engine_name_cstr = None;
-
-        let application_info = create_info.application_info.map(|a| {
-            let application_name_ptr = match a.application_name {
-                Some(application_name) => {
-                    let tmp = CString::new(application_name).unwrap();
-                    let ptr = tmp.as_ptr();
-                    application_name_cstr = Some(tmp);
-                    ptr
-                }
-
-                None => ptr::null(),
-            };
-
-            let engine_name_ptr = match a.engine_name {
-                Some(engine_name) => {
-                    let tmp = CString::new(engine_name).unwrap();
-                    let ptr = tmp.as_ptr();
-                    engine_name_cstr = Some(tmp);
-                    ptr
-                }
-
-                None => ptr::null(),
-            };
-
-            let api_version = match a.api_version {
-                Some(version) => vk_sys::vk_make_version(version.major as u32, version.minor as u32, version.patch as u32),
-                None => 0,
-            };
-
-            vk_sys::VkApplicationInfo {
-                sType: vk_sys::VK_STRUCTURE_TYPE_APPLICATION_INFO,
-                pNext: ptr::null(),
-                pApplicationName: application_name_ptr,
-                applicationVersion: a.application_version,
-                pEngineName: engine_name_ptr,
-                engineVersion: a.engine_version,
-                apiVersion: api_version,
-            }
-        });
-
-        let application_info_ptr = match application_info {
-            Some(ref application_info) => application_info,
-            None => ptr::null(),
-        };
-
-        let enabled_layer_names_cstr: Vec<_> = create_info.enabled_layers
-            .drain(..)
-            .map(|l| CString::new(l).unwrap())
-            .collect();
-        let enabled_layer_names_ptrs: Vec<_> = enabled_layer_names_cstr
-            .iter()
-            .map(|l| l.as_ptr())
-            .collect();
-
-        let enabled_extension_names_cstr: Vec<_> = create_info.enabled_extensions
-            .drain(..)
-            .map(|e| CString::new(String::from(e).into_bytes()).unwrap())
-            .collect();
-        let enabled_extension_names_ptrs: Vec<_> = enabled_extension_names_cstr
-            .iter()
-            .map(|l| l.as_ptr())
-            .collect();
-
-        let create_info = vk_sys::VkInstanceCreateInfo {
-            sType: vk_sys::VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-            pNext: ptr::null(),
-            flags: create_info.flags,
-            pApplicationInfo: application_info_ptr,
-            enabledLayerCount: enabled_layer_names_ptrs.len() as u32,
-            ppEnabledLayerNames: enabled_layer_names_ptrs.as_ptr(),
-            enabledExtensionCount: enabled_extension_names_ptrs.len() as u32,
-            ppEnabledExtensionNames: enabled_extension_names_ptrs.as_ptr(),
-        };
-
+    pub fn create(create_info: core::InstanceCreateInfo, allocator: Option<Box<core::Allocator>>) -> Result<Instance> {
         let allocator_helper = allocator.map(AllocatorHelper::new);
         let allocation_callbacks = allocator_helper.as_ref().map_or(ptr::null(), |a| &a.callbacks);
 
@@ -111,9 +35,11 @@ impl Instance {
             loader.load_core_null_instance();
         }
 
+        let create_info: core::VkInstanceCreateInfoWrapper = create_info.into();
+        let create_info: &vk_sys::VkInstanceCreateInfo = &create_info;
         let mut instance = ptr::null_mut();
         let res = unsafe {
-            (loader.core_null_instance.vkCreateInstance)(&create_info, allocation_callbacks, &mut instance)
+            (loader.core_null_instance.vkCreateInstance)(create_info, allocation_callbacks, &mut instance)
         };
         if res != vk_sys::VK_SUCCESS {
             return Err(res.into());
