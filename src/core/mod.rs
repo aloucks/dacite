98bf628af464552f02c7e0ad91a29cb075a8e727
Dennis Hamester <dennis.hamester@startmail.com>
2017-04-30 13:07:26 +0200

    core: Add missing From implementation for DeviceExtensionProperties

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2051,6 +2051,22 @@ impl<'a> From<&'a vk_sys::VkExtensionProperties> for DeviceExtensionProperties {
     }
 }
 
+impl<'a> From<&'a DeviceExtensionProperties> for vk_sys::VkExtensionProperties {
+    fn from(properties: &'a DeviceExtensionProperties) -> Self {
+        unsafe {
+            let name: &str = (&properties.extension).into();
+            debug_assert!(name.len() < vk_sys::VK_MAX_EXTENSION_NAME_SIZE);
+
+            let mut res: vk_sys::VkExtensionProperties = mem::uninitialized();
+            ptr::copy_nonoverlapping(name.as_ptr() as *const _, res.extensionName.as_mut_ptr(), name.len());
+            res.extensionName[name.len()] = 0;
+            res.specVersion = properties.spec_version;
+
+            res
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct LayerProperties {
     pub layer_name: String,
