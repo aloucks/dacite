88e8e01aae7fadf2184199c24a82504ccd1413ed
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-02 11:55:30 +0200

    command_pool: Implement allocate_command_buffers

--- src/core/command_pool.rs
+++ src/core/command_pool.rs
@@ -13,8 +13,8 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 use Result;
-use core::Device;
 use core::allocator_helper::AllocatorHelper;
+use core::{self, CommandBuffer, Device};
 use std::ptr;
 use std::sync::Arc;
 use vk_sys;
@@ -73,6 +73,27 @@ impl CommandPool {
 
         if res == vk_sys::VK_SUCCESS {
             Ok(())
+        }
+        else {
+            Err(res.into())
+        }
+    }
+
+    pub fn allocate_command_buffers(&self, allocate_info: &core::CommandBufferAllocateInfo) -> Result<Vec<CommandBuffer>> {
+        let mut command_buffers = Vec::with_capacity(allocate_info.command_buffer_count as usize);
+        unsafe {
+            command_buffers.set_len(allocate_info.command_buffer_count as usize);
+        }
+
+        let mut allocate_info: core::VkCommandBufferAllocateInfoWrapper = allocate_info.into();
+        allocate_info.set_command_pool(Some(self.clone()));
+
+        let res = unsafe {
+            (self.loader().core.vkAllocateCommandBuffers)(self.device_handle(), allocate_info.as_ref(), command_buffers.as_mut_ptr())
+        };
+
+        if res == vk_sys::VK_SUCCESS {
+            Ok(command_buffers.iter().map(|&c| CommandBuffer::new(c, self.clone())).collect())
         }
         else {
             Err(res.into())
