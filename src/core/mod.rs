bb882ed749b2a03a1fd64be8eee509b12a6a555b
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-02 15:02:51 +0200

    core: Add Buffer

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -13,6 +13,7 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 mod allocator_helper;
+mod buffer;
 mod command_buffer;
 mod command_pool;
 mod device;
@@ -34,6 +35,7 @@ use std::slice;
 use utils;
 use vk_sys;
 
+pub use self::buffer::Buffer;
 pub use self::command_buffer::CommandBuffer;
 pub use self::command_pool::CommandPool;
 pub use self::device::Device;
