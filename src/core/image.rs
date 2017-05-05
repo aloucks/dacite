90140c086d86f4f5d1e03589c509f7cae23e97b1
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-05 19:08:45 +0200

    image: Implement AsNativeVkObject

--- src/core/image.rs
+++ src/core/image.rs
@@ -12,6 +12,7 @@
 // OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+use AsNativeVkObject;
 use core::Device;
 use core::allocator_helper::AllocatorHelper;
 use std::ptr;
@@ -40,6 +41,15 @@ impl Drop for Inner {
 
 #[derive(Debug, Clone)]
 pub struct Image(Arc<Inner>);
+
+impl AsNativeVkObject for Image {
+    type NativeVkObject = vk_sys::VkImage;
+
+    #[inline]
+    fn as_native_vk_object(&self) -> Self::NativeVkObject {
+        self.handle()
+    }
+}
 
 impl Image {
     pub(crate) fn new(handle: vk_sys::VkImage, device: Device, allocator: Option<AllocatorHelper>) -> Self {
