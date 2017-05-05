2585254c79259696301d13025d5ce230acc045ba
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-05 19:14:28 +0200

    pipeline_layout: Implement AsNativeVkObject

--- src/core/pipeline_layout.rs
+++ src/core/pipeline_layout.rs
@@ -12,6 +12,7 @@
 // OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+use AsNativeVkObject;
 use core::Device;
 use core::allocator_helper::AllocatorHelper;
 use std::ptr;
@@ -40,6 +41,15 @@ impl Drop for Inner {
 
 #[derive(Debug, Clone)]
 pub struct PipelineLayout(Arc<Inner>);
+
+impl AsNativeVkObject for PipelineLayout {
+    type NativeVkObject = vk_sys::VkPipelineLayout;
+
+    #[inline]
+    fn as_native_vk_object(&self) -> Self::NativeVkObject {
+        self.handle()
+    }
+}
 
 impl PipelineLayout {
     pub(crate) fn new(handle: vk_sys::VkPipelineLayout, device: Device, allocator: Option<AllocatorHelper>) -> Self {
