452978b7b7ff26f3320416a65176007c76217c9a
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-05 19:12:59 +0200

    pipeline_cache: Implement AsNativeVkObject

--- src/core/pipeline_cache.rs
+++ src/core/pipeline_cache.rs
@@ -12,6 +12,7 @@
 // OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+use AsNativeVkObject;
 use core::Device;
 use core::allocator_helper::AllocatorHelper;
 use std::ptr;
@@ -40,6 +41,15 @@ impl Drop for Inner {
 
 #[derive(Debug, Clone)]
 pub struct PipelineCache(Arc<Inner>);
+
+impl AsNativeVkObject for PipelineCache {
+    type NativeVkObject = vk_sys::VkPipelineCache;
+
+    #[inline]
+    fn as_native_vk_object(&self) -> Self::NativeVkObject {
+        self.handle()
+    }
+}
 
 impl PipelineCache {
     pub(crate) fn new(handle: vk_sys::VkPipelineCache, device: Device, allocator: Option<AllocatorHelper>) -> Self {
