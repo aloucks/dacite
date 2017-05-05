69e04fa91400e4fc50a5ea69b189b5668ebc3473
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-05 19:08:10 +0200

    framebuffer: Implement AsNativeVkObject

--- src/core/framebuffer.rs
+++ src/core/framebuffer.rs
@@ -12,6 +12,7 @@
 // OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+use AsNativeVkObject;
 use core::Device;
 use core::allocator_helper::AllocatorHelper;
 use std::ptr;
@@ -40,6 +41,15 @@ impl Drop for Inner {
 
 #[derive(Debug, Clone)]
 pub struct Framebuffer(Arc<Inner>);
+
+impl AsNativeVkObject for Framebuffer {
+    type NativeVkObject = vk_sys::VkFramebuffer;
+
+    #[inline]
+    fn as_native_vk_object(&self) -> Self::NativeVkObject {
+        self.handle()
+    }
+}
 
 impl Framebuffer {
     pub(crate) fn new(handle: vk_sys::VkFramebuffer, device: Device, allocator: Option<AllocatorHelper>) -> Self {
