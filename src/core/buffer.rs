1c557f8255874d8f14ab3db9da00c6535d50b184
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-11 19:24:37 +0200

    buffer: Add loader() and device_handle()

--- src/core/buffer.rs
+++ src/core/buffer.rs
@@ -57,6 +57,16 @@ impl Buffer {
     pub(crate) fn handle(&self) -> vks::VkBuffer {
         self.0.handle
     }
+
+    #[inline]
+    pub(crate) fn loader(&self) -> &vks::DeviceProcAddrLoader {
+        self.0.device.loader()
+    }
+
+    #[inline]
+    pub(crate) fn device_handle(&self) -> vks::VkDevice {
+        self.0.device.handle()
+    }
 }
 
 #[derive(Debug)]
