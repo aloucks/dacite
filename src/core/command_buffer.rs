d7d928abfa5098e75ff1f245f072c092e127cd3f
Dennis Hamester <dennis.hamester@startmail.com>
2017-05-13 21:19:48 +0200

    command_buffer: Implement blit_image()

--- src/core/command_buffer.rs
+++ src/core/command_buffer.rs
@@ -271,6 +271,14 @@ impl CommandBuffer {
             (self.loader().core.vkCmdCopyImage)(self.handle(), src_image.handle(), src_image_layout.into(), dst_image.handle(), dst_image_layout.into(), regions.len() as u32, regions.as_ptr());
         }
     }
+
+    /// See [`vkCmdBlitImage`](https://www.khronos.org/registry/vulkan/specs/1.0-extensions/html/vkspec.html#vkCmdBlitImage)
+    pub fn blit_image(&self, src_image: &Image, src_image_layout: core::ImageLayout, dst_image: &Image, dst_image_layout: core::ImageLayout, regions: &[core::ImageBlit], filter: core::Filter) {
+        let regions: Vec<_> = regions.iter().map(From::from).collect();
+        unsafe {
+            (self.loader().core.vkCmdBlitImage)(self.handle(), src_image.handle(), src_image_layout.into(), dst_image.handle(), dst_image_layout.into(), regions.len() as u32, regions.as_ptr(), filter.into());
+        }
+    }
 }
 
 #[derive(Debug)]
